b3d869956a8b2cffc434cb9d221d6eac2cdfbdda
Dakssh bhambre <Dakssh.bhambre@gmail.com>
2024-08-11 18:20:19 +0530

    Add login UI

---
+++ script.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Q5C-rHwqJTHdkrkfr3TaHx_O8l2A50Y",
+  authDomain: "wchat-55495.firebaseapp.com",
+  projectId: "wchat-55495",
+  storageBucket: "wchat-55495.appspot.com",
+  messagingSenderId: "414607502356",
+  appId: "1:414607502356:web:e420d3d0a27a36f437cb01",
+  measurementId: "G-K8YEQ2V53R"
+};
+
+const app = initializeApp(firebaseConfig);
